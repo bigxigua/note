0bbd4bd97bba5b321bac9a0764432ae8901fdac6
bigxigua <1336945839@qq.com>
2019-12-21 18:47:07 +0800

    bugfix: 修复页面不能滑动问题

--- app/page/index/index.js
+++ app/page/index/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '@components/header/header.js';
 import SiderBarLayout from '@components/sider-bar';
+import Footer from '@components/footer';
 import ContentLayout from '@components/content-layout';
 import RightPanelLayout from '@components/right-panel-layout';
 import './index.css';
@@ -14,6 +15,7 @@ export default function Index() {
         <ContentLayout />
         <RightPanelLayout />
       </div>
+      <Footer />
     </div>
   );
 }
