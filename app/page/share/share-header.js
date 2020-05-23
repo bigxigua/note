4d2c3f21e85a30ca9722314c433b6117127e7576
bigxigua <1336945839@qq.com>
2020-05-23 22:29:39 +0800

    feat: 新增分享页【访问原文档入口】

--- app/page/share/share-header.js
+++ app/page/share/share-header.js
@@ -1,6 +1,18 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useContext, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+import Button from '@common/button';
+import userContext from '@context/user/userContext';
 
 export default function ShareHeader({ docInfo = {} }) {
+  const { userInfo = {} } = useContext(userContext);
+  const isSelf = userInfo.uuid && docInfo.uuid && docInfo.uuid === userInfo.uuid;
+  const history = useHistory();
+
+  const onButtonClick = useCallback(() => {
+    const { space_id, doc_id } = docInfo;
+    history.push(`/article/${doc_id}?spaceId=${space_id}`);
+  }, [docInfo]);
+
   return <div className="share-header">
     <div className="share-header__content">
       <div className="share-header__left">
@@ -8,7 +20,15 @@ export default function ShareHeader({ docInfo = {} }) {
           href="/"></a>
         <span>{docInfo.title}</span>
       </div>
-      <div className="share-header__right"></div>
+      <div className="share-header__right">
+        {
+          isSelf && <Button
+            content="查看原始文档"
+            style={{ marginLeft: '20px' }}
+            onClick={onButtonClick}
+            type="primary" />
+        }
+      </div>
     </div>
   </div>;
 }
