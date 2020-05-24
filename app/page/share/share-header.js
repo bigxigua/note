c0e68802aa0e949813cb360e1556d56d6d5c0e6f
bigxigua <1336945839@qq.com>
2020-05-24 13:18:15 +0800

    feat: 新增一些功能

--- app/page/share/share-header.js
+++ app/page/share/share-header.js
@@ -2,18 +2,20 @@ import React, { useContext, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '@common/button';
 import userContext from '@context/user/userContext';
+import { checkBrowser } from '@util/util';
 
 export default function ShareHeader({ docInfo = {} }) {
   const { userInfo = {} } = useContext(userContext);
   const isSelf = userInfo.uuid && docInfo.uuid && docInfo.uuid === userInfo.uuid;
   const history = useHistory();
+  const { isMobile } = checkBrowser();
 
   const onButtonClick = useCallback(() => {
     const { space_id, doc_id } = docInfo;
     history.push(`/article/${doc_id}?spaceId=${space_id}`);
   }, [docInfo]);
 
-  return <div className="share-header">
+  return <div className={$.trim(`share-header ${isMobile ? 'share-header__mobile' : ''}`)}>
     <div className="share-header__content">
       <div className="share-header__left">
         <a className="share-header__logo"
